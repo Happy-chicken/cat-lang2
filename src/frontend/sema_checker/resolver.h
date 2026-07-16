5719b40fb4e6f00d947a31676d8f3b20f0ebf3f7
bu yi <yi.bu@rwth-aachen.de>
2026-07-16 18:54:46 +0800

    add decl of resolver

--- src/frontend/sema_checker/resolver.h
+++ src/frontend/sema_checker/resolver.h
@@ -8,5 +8,23 @@ class Resolver : public semantics::Pass {
   const char *name() const noexcept { return "Resolver"; }
 
   bool run(Program &program, semantics::SemaCtxt &ctx, error::DiagCtxt &diag);
+
+  void declare_type_item(const ItemNode &item_node, semantics::SemaCtxt &ctx,
+                         error::DiagCtxt &diag);
+
+  void declare_trait_item(const ItemNode &item_node, semantics::SemaCtxt &ctx,
+                          error::DiagCtxt &diag);
+
+  void declare_value_item(const ItemNode &item_node, semantics::SemaCtxt &ctx,
+                          error::DiagCtxt &diag);
+
+  void declare_impl_methods(const ItemNode &item_node, semantics::SemaCtxt &ctx,
+                            error::DiagCtxt &diag);
+
+  void declare_global_var(const ItemNode &item_node, semantics::SemaCtxt &ctx,
+                          error::DiagCtxt &diag);
+
+  void validate_impl_target(Impl &impl, Span span, semantics::SemaCtxt &ctx,
+                            error::DiagCtxt &diag);
 };
 } // namespace cat
